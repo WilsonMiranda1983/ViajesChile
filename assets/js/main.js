9a07b7cd05c9b1ffc0c60c133a6802156f8a126e
wilsonmiranda1983 <wilson.m.1983@gmail.com>
2024-02-17 03:32:22 -0300

    Se Inicia Proyecto

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+$(function(){
+
+    //componente que activa tooltips//
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+$(".navbar a").click(function(event){ //Funcion que realiza el SmoothScroll//
+    if (this.has !== "") {
+        event.preventDefault();
+
+        var gato = this.hash;
+
+        $("html, body").animate({
+            scrolltop: $(gato).offset().top
+        }, 800, function(){
+            window.location.hash = gato;
+        });
+    }
+});
+
+
+$(window).scroll(function () {  //funcion para cambiar color de navbar//
+    if ($("#navbar").offset().top > 56) {
+        $("#navbar").addClass("navbar-color");
+    } else {
+        $("#navbar").removeClass("navbar-color");
+    }
+});
+
+
+
+$("#btn_correo").click(function () { // Funcion que muestra mensaje confirmando envio de mensaje//
+    alert("El correo fue enviado correctamente...");
+});
+
+
+})
